87741d707b3a71d3e115e39cd894c20955c913a4
jameem <jameem@findingstorage.com>
2019-04-17 19:59:33 +0530

    Heroku deploy

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
+var express = require("express");
 var http = require("http");
 var fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 
-fs.readFile("./src/index.html", function(err, html) {
-  if (err) throw err;
+app = express();
 
-  http
-    .createServer(function(request, response) {
-      response.writeHeader(200, { "Content-Type": "text/html" });
-      response.write(html);
-      response.end();
-    })
-    .listen(PORT);
+app.use(express.static(__dirname + "/src"));
+
+app.get("/", function(req, res) {
+  res.sendfile("index.html");
 });
+
+app.listen(PORT);
+
+// fs.readFile("./src/index.html", function(err, html) {
+//   if (err) throw err;
+//
+//   http
+//     .createServer(function(request, response) {
+//       response.writeHeader(200, { "Content-Type": "text/html" });
+//       response.write(html);
+//       response.end();
+//     })
+//     .listen(PORT);
+// });
